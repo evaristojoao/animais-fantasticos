041582cdc5f229e971a7d89a7ad50edbf5ec2275
João Eugénio <99355521+JoaoEvaristo23@users.noreply.github.com>
2025-04-15 08:25:51 +0100

    accordion transformed into class

--- js/modules/accordion.js
+++ js/modules/accordion.js
@@ -1,20 +1,28 @@
-// Função initAccordion para configurar a funcionalidade de acordeão, comum em listas de FAQ.
-export default function initAccordion() { 
-    const accordionList = document.querySelectorAll('[data-anime="accordion"] dt');
-    const activeClass = 'ativo';
+export default class Accordion {
+    constructor(list) {
+        this.accordionList = document.querySelectorAll(list);
+        this.activeClass = 'ativo';
+    }
 
-    if (accordionList.length) { // Verifica se existem elementos <dt> na lista. Se houver pelo menos um, o código interno é executado.
-        accordionList[0].classList.add(activeClass);
-        accordionList[0].nextElementSibling.classList.add(activeClass); // Adiciona a classe ativo ao elemento seguinte do primeiro <dt> (geralmente um <dd>, que contém a resposta), fazendo-o ficar visível.
+    toggleAccordion(item) {
+        item.classList.toggle(this.activeClass);
+        item.nextElementSibling.classList.toggle(this.activeClass);
+    }
 
-        // Função interna activeAccordion, que será executada quando um <dt> for clicado.
-        function activeAccordion() {
-            this.classList.toggle(activeClass); // Utiliza toggle para adicionar ou remover a classe ativo no elemento <dt> que foi clicado (this refere-se ao elemento atual).
-            this.nextElementSibling.classList.toggle(activeClass); // Aplica o mesmo toggle no próximo elemento irmão do <dt> (o <dd>), alternando sua visibilidade.
-        }
-
-        accordionList.forEach((item) => {
-            item.addEventListener('click',activeAccordion);
+    // adiciona os eventos ao accordion
+    addAccordionEvent() {
+        this.accordionList.forEach((item) => {
+            item.addEventListener('click',() => this.toggleAccordion(item));
         });
+    }
+
+    // iniciar funçao
+    init() {
+        if (this.accordionList.length) {
+            // ativar primeiro item
+            this.toggleAccordion(this.accordionList[0]);
+            this.addAccordionEvent();
+        }
+        return this;
     }
 }
