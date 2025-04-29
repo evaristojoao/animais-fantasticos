6fefe6955a35617fcd11c0125d9bcd0c926946df
João Eugénio <joaoeugenio20@gmail.com>
2025-04-29 17:12:34 +0100

    Scroll-suave formatting

--- js/modules/scroll-suave.js
+++ js/modules/scroll-suave.js
@@ -1,34 +1,35 @@
 export default class ScrollSuave {
-    constructor(links,options) {
-        this.linksInternos = document.querySelectorAll(links);
-        if (options === undefined) {
-            this.options = { behavior: 'smooth',block: 'start' };
-        } else {
-            this.options = options;
-        }
+  constructor(links, options) {
+    this.linksInternos = document.querySelectorAll(links);
 
-        this.scrollToSection = this.scrollToSection.bind(this);
+    if (options === undefined) {
+      this.options = { behavior: "smooth", block: "start" };
+    } else {
+      this.options = options;
     }
 
-    scrollToSection(event) {
-        event.preventDefault();
-        const href = event.currentTarget.getAttribute('href');
-        const section = document.querySelector(href);
-        section.scrollIntoView(this.options);
-    }
+    this.scrollToSection = this.scrollToSection.bind(this);
+  }
 
-    addLinkEvent() {
-        this.linksInternos.forEach((link) => {
-            link.addEventListener('click', (event) => { 
-                this.scrollToSection(event);
-             });
-        });
-    }
+  scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+    section.scrollIntoView(this.options);
+  }
 
-    init() {
-        if (this.linksInternos.length) {
-            this.addLinkEvent();
-        }
-        return this;
+  addLinkEvent() {
+    this.linksInternos.forEach((link) => {
+      link.addEventListener("click", (event) => {
+        this.scrollToSection(event);
+      });
+    });
+  }
+
+  init() {
+    if (this.linksInternos.length) {
+      this.addLinkEvent();
     }
+    return this;
+  }
 }
